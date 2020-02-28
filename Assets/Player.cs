4947e715ae7f19230df6e32815e23add46cbea2f
Matti Petrelius <matti.petrelius@gmail.com>
2020-02-28 19:20:48 +0200

    Dude is running

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            this.GetComponent<Rigidbody>().velocity = Vector3.left * speed;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            this.GetComponent<Rigidbody>().velocity = Vector3.right * speed;
+        }
+    }
+}
