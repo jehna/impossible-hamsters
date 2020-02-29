dce1067ce584a53e8c16f67981085672ccc822fe
Jesse Luoto <jesse.luoto@reaktor.com>
2020-02-29 15:33:59 +0200

    Add clouds

---
+++ Assets/MoveByWind.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByWind : MonoBehaviour
+{
+    private Wind wind;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wind = FindObjectOfType<Wind>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * wind.direction * speed);
+    }
+}
