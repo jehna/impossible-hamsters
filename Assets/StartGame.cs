6c2743b00e895a03c729ae5a51c3a47a90cf9f7a
Matti Petrelius <matti.petrelius@gmail.com>
2020-02-29 17:33:28 +0200

    Add start screen

---
+++ Assets/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<Image>().enabled = false;
+            Time.timeScale = 1;
+        }
+    }
+}
