11de7519e3a447eed3beba8d6802fd7400e97b44
Jesse Luoto <jesse.luoto@reaktor.com>
2020-02-29 12:26:00 +0200

    Spawn random new players

---
+++ Assets/PlayerSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public Transform playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNewPlayer();
+    }
+
+    // Update is called once per frame
+    void SpawnNewPlayer()
+    {
+        Instantiate(playerPrefab);
+        Invoke("SpawnNewPlayer", Random.Range(5.0f, 20.0f));
+    }
+}
