f5809a7ff8f2c2b2d5e01e4182b3eb6815c82268
Matti Petrelius <matti.petrelius@gmail.com>
2020-03-01 12:33:42 +0200

    Add wind sounds

--- Assets/Wind.cs
+++ Assets/Wind.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 
 public class Wind : MonoBehaviour
 {
-    public float direction = -1;
+    public float direction = 0;
+    float prevDirection = 0;
     float elapsedSinceChange = 0;
+    public float strongWindLimit = 0.7f;
+    public AudioSource leftWindSound;
+    public AudioSource rightWindSound;
 
     public float windChangeScale = 0.3f;
     // Start is called before the first frame update
@@ -20,18 +24,24 @@ public class Wind : MonoBehaviour
     {
         this.elapsedSinceChange += Time.deltaTime;
 
+        this.prevDirection = this.direction;
         this.direction = (Mathf.PerlinNoise(Time.time * windChangeScale, 0) * 2) - 1;
 
-        string text = "";
+        // string text = "";
 
-        if (direction < 0) {
-            text = "<" + direction;
-        } else {
-            text = ">" + direction;
+        // if (direction < 0) {
+        //     text = "<" + direction;
+        // } else {
+        //     text = ">" + direction;
+        // }
+
+        // this.GetComponent<Text>().text = text;
+
+        if (prevDirection < strongWindLimit && direction > strongWindLimit) {
+            this.rightWindSound.Play();
+        } else if (prevDirection > -strongWindLimit && direction < -strongWindLimit) {
+            this.leftWindSound.Play();
         }
 
-        this.GetComponent<Text>().text = text;
-
-        
     }
 }
